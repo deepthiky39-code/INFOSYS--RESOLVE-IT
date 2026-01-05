7d1406448df51d98b3d349f745984954d76b355c
DEEPTHI K Y <235835060+deepthiky39-code@users.noreply.github.com>
2026-01-05 20:25:47 +0530

    Delete backend-java directory

--- backend-java/src/main/java/com/grievance/repository/FeedbackRepository.java
+++
@@ -1,7 +0,0 @@
-package com.grievance.repository;
-
-import com.grievance.model.Feedback;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
-}
